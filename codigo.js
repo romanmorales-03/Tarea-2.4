2808d1702c97234a406bad715c03e3ab244281ef
romanmorales-03 <daniel.morales.torres@u-erre.mx>
2025-07-03 18:06:43 -0600

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -13,10 +13,9 @@ localStorage.setItem("datos",JSON.stringify(lista));
 usuarios=JSON.parse(localStorage.getItem("datos"));
 console.log(typeof(usuarios));
 
-
 function logo(){
-
-
-}
-
-
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  
+  const validUser = usuarios.find(user => user.username === username && user.password === password)
+  if(!validUser) { return alert('Usuario y/o contraseña incorrectos!')}
